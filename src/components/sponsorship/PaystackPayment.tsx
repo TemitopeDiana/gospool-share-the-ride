b483dd13d26cd708421a40a42eb55c345176f867
Temiloluwa <ogunbanjotemiloluwa@gmail.com>
2025-08-26 15:38:28 +0100

    Fixes

--- src/components/sponsorship/PaystackPayment.tsx
+++ src/components/sponsorship/PaystackPayment.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { useEffect, useCallback, useRef, useState } from 'react';
 import { usePaystackPayment } from '@/hooks/usePaystackPayment';
